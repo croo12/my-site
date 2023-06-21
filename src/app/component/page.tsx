05b38dd2320c6fb777d28ccba4cf15a7ef7e1211
jiwoo lee <its19447@gmail.com>
2023-06-21 12:51:09 +0900

    글 등록 기능

--- src/app/component/page.tsx
+++ src/app/component/page.tsx
@@ -4,6 +4,7 @@ const ComponentPage = () => {
   return (
     <>
       <div>
+        <h1>이 페이지는 공사중 입니다.</h1>
         <CardComponent style={{ width: "33%" }}>
           <div>반가워요</div>
         </CardComponent>
